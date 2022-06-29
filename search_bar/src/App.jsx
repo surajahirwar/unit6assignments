b866b764337304229fb15c8f288a19dae31a5172
Suraj Ahirwar <75873682+surajahirwar@users.noreply.github.com>
2022-06-29 15:01:23 +0530

    search bar

--- search_bar/src/App.jsx
+++ search_bar/src/App.jsx
@@ -7,25 +7,43 @@ import './App.css'
 function App() {
 
   const [data, setdata] = useState("")
-
+  const  [append, setappend] = useState([])
   let inputHandler = (e) => {
     var lowerCase = e.target.value;
     setdata(lowerCase);
   };
 
   useEffect(()=>{
-        axios.get(`http://localhost:8080/data/?q=${data}`).then((e)=> console.log(e.data))
+        if(data.length >=2){
+          axios.get(`http://localhost:8080/data?q=${data}`)
+          .then((e)=> 
+            setappend(e.data)
+          )
+        }
+        else{
+         
+        }
+   
+        
+  },[data])
 
-      // getdata()
-  },data)
-
+  // console.log(append)
   
   return (
     <div className="App">
-        hello
+       <h1>search country</h1>
         <div>
           <input type="text" onChange={inputHandler}  placeholder='search here' />
+          {data.length > 1 ? <div> {append.map((e)=>(
+            
+            <div key={e.country}>
+              <p>{e.country}</p>
+            </div>
+          ))} </div>:""
+        }
+          
         </div>
+
     </div>
   )
 }
