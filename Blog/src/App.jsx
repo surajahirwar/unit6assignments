df089647f661984119759c5be7fffc771b011886
Suraj Ahirwar <75873682+surajahirwar@users.noreply.github.com>
2022-07-04 23:30:43 +0530

    blog

---
+++ Blog/src/App.jsx
@@ -0,0 +1,18 @@
+import Header from "./components/header/Header"
+import Home from "./components/home/Home"
+
+function App() {
+
+  return (
+    <div className="App">
+
+    <Header />
+
+    <Home />      
+
+
+    </div>
+  )
+}
+
+export default App
---
+++ Blog/src/components/header/Header.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import "./Style.css";
+export default function Header() {
+  return (
+    <div>
+        
+<header>
+  <nav className="navbar navbar-expand-md navbar-dark fixed-top main_navbar ">
+    <div className="container-fluid">
+      <a className="navbar-brand" href="#">Logo</a>
+      <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+        <span className="navbar-toggler-icon"></span>
+      </button>
+      <div className="collapse navbar-collapse navbar_ul" id="navbarCollapse">
+        <ul className="navbar-nav me-auto mb-3 mr-50 mb-md-0">
+          <li className="nav-item">
+            <a style={{color:"black"}} className="nav-link active dark" aria-current="page" href="#">Home</a>
+          </li>
+          <li className="nav-item">
+            <a style={{color:"black"}} className="nav-link active" aria-current="page" href="#">View</a>
+          </li>
+          <li className="nav-item">
+            <a style={{color:"black"}} className="nav-link active" aria-current="page" href="#">About</a>
+          </li>
+          <li className="nav-item">
+            <a style={{color:"black"}} className="nav-link active" aria-current="page" href="#">Contect Us</a>
+          </li>
+        </ul>
+        
+          <button className="btn btn-outline-success" type="submit">Search</button>
+       
+      </div>
+    </div>
+  </nav>
+</header>
+
+    </div>
+  )
+}
